4f91454739908565543f28d36415574ba2312a67
Alexander Ikenga <alexanderikenga@gmail.com>
2022-10-23 17:49:52 -0700

    prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-		_putchar('\n')
+		_putchar('\n');
 	}
 	else
 	{
@@ -20,7 +20,7 @@ void print_square(int size)
 			{
 				_putchar('#');
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
